6381233e74f431765d350edc44c70577cc07f9fd
Adrian <adrmalec@proton.me>
2024-04-07 11:01:23 +0200

    chore:poprawa kodu

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 
     public class Main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie go wartościami
+       
         Point point = new Point(4.0, 6.0);
 
-        // Wyświetlanie współrzędnych punktu
+       
         System.out.println("Współrzędne punktu:");
         System.out.println("x: " + point.getX());
         System.out.println("y: " + point.getY());
