7e7c83d96b20c5b66a83b43e9f17f5faf5a27c21
Adrian <adrmalec@proton.me>
2024-04-07 10:59:20 +0200

    chore:poprawa zadania

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
     public class Main {
     public static void main(String[] args) {
         // Tworzenie obiektu klasy Point i inicjowanie go wartościami
-        Point point = new Point(3.5, 2.0);
+        Point point = new Point(4.0, 6.0);
 
         // Wyświetlanie współrzędnych punktu
         System.out.println("Współrzędne punktu:");
