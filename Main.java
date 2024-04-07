322c05d21dea0c9281a30bb621088cb8b8f90280
Adrian <adrmalec@proton.me>
2024-04-07 12:37:25 +0200

    chore:zadanie6

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(6, 12);
-        
-        System.out.println("Wyniki_kola:");
-        System.out.println("Dlugosc: " + rectangle.getLength());
-        System.out.println("Szerokosc: " + rectangle.getWidth());
-        System.out.println("Pole_prostokata: " + rectangle.calculateArea());
-        System.out.println("Obwod_prostokata: " + rectangle.calculatePerimeter());
+        Square square = new Square(8);
+
+
+        System.out.println("Wyniki_kwadratu:");
+        System.out.println("Dlugosc_boku: " + square.getLength());
+        System.out.println("Pole_kwadratu: " + square.calculateArea());
+        System.out.println("Obwod_kwadratu: " + square.calculatePerimeter());
     }
 }
